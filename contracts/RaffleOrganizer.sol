4e37709c7921341ed18c133f9b11de13ce895f97
laxmansharma1 <silvercenturion1999@gmail.com>
2022-06-01 17:45:17 +0530

    added getallraffles

--- contracts/RaffleOrganizer.sol
+++ contracts/RaffleOrganizer.sol
@@ -2,21 +2,37 @@
 pragma solidity >=0.4.21 <8.10.0;
 
 contract RaffleOrganizer {
+  // Think of storing this data to offchain
   struct Raffle 
   {
     string Name;
     string description;
   }
 
-  mapping(address => Raffle) raffles;
+  mapping(address => Raffle) public raffles;
+  address[] public creatorAddresses;
 
   function addRaffle(string memory _name, string memory _description) public {
     raffles[msg.sender] = Raffle(_name, _description);
+    creatorAddresses.push(msg.sender);
   }
 
   function getRaffle(address _ownerAddress) public view returns(string memory, string memory) {
     return (raffles[_ownerAddress].Name, raffles[_ownerAddress].description);
   }
+  // work arround for returning mapping
+  function getAllRaffle() public view returns (address[] memory, Raffle[] memory) {
+
+    address[] memory raffleCreatorAddresses = new address[](creatorAddresses.length);
+    Raffle[] memory raffleInfo = new Raffle[](creatorAddresses.length); 
+    for(uint i = 0; i < creatorAddresses.length; i++) {
+      raffleCreatorAddresses[i] = creatorAddresses[i]; 
+      raffleInfo[i] = raffles[creatorAddresses[i]]; 
+    }
+    return (raffleCreatorAddresses, raffleInfo);
+
+  }
+
 }
 
 /*
