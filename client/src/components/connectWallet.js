341a3f0042945a0acf6f0e5332e8721d8eb04dff
dsingh <dsingh@commvault.com>
2022-06-02 21:25:06 +0530

    persistent login status for connect wallet

--- client/src/components/connectWallet.js
+++ client/src/components/connectWallet.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Button, TextField, Grid } from "@mui/material";
 import { ethers } from "ethers";
 import { EtherscanProvider } from "@ethersproject/providers";
@@ -7,27 +7,6 @@ const ConnectWallet = () => {
 
     const [address, setAddress] = useState('');
     const [loginStatus, setloginStatus] = useState(false);
-
-    const checkIfWalletIsConnected = async () => {
-        try {
-            const { ethereum } = window; // metamask injects ethereum object into window
-            if(!ethereum) {
-                alert("There is no metamask wallet");
-            }
-
-            const accounts = await ethereum.request({method: "eth_accounts"}); 
-            if(accounts.length !== 0) {
-                const account = accounts[0]; 
-                setAddress(account); 
-                setloginStatus(true);
-            } else {
-                alert("No authorised account is found");
-            }
-        } catch (error) {
-            alert("Something went wrong");
-            console.log(error);
-        }
-    }
 
     const connectWallet = async () => {
         try{
@@ -46,17 +25,51 @@ const ConnectWallet = () => {
         }
     }
 
-    useEffect(() => {
-        connectWallet();
-        checkIfWalletIsConnected();   
-    }, [])
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const { ethereum } = window; // metamask injects ethereum object into window
+            if(!ethereum) {
+                alert("There is no metamask wallet");
+            }
 
+            const accounts = await ethereum.request({method: "eth_accounts"}); 
+            console.log(accounts);
+            if(accounts.length !== 0) {
+                const account = accounts[0]; 
+                setAddress(account); 
+                setloginStatus(true);
+            } else {
+                connectWallet();
+            }
+        } catch (error) {
+            alert("Something went wrong");
+            console.log(error);
+        }
+    }
+
+    const shorterAddress =  useMemo(() => {
+        if(address.length < 12) {
+            return address;
+        }
+
+        return `${address.substring(0,6)}...${address.substring(address.length - 4)}`
+
+    }, [address])
+
+    useEffect(() => {
+        checkIfWalletIsConnected();   
+    }, [address])
+
+   
     return (
         <>
-        {!loginStatus && (<Grid container justifyContent='right' padding='10px 10px 10px 10px'>
-            <Button variant="contained" color='secondary' align='right' onClick={connectWallet}> Connect Wallet </Button>
-        </Grid>)}
-        {loginStatus && (<h1>{address}</h1>)}
+        <Grid container justifyContent='right' padding='10px 10px 10px 10px'>
+        {!loginStatus && (
+            <Button variant="contained" color='secondary' align='right' onClick={connectWallet}> Connect Wallet </Button>)}
+        {loginStatus && (
+            <Button variant="outlined" color='secondary' align='right'>{shorterAddress}</Button>
+        )}
+        </Grid>
         </>
     )
 }
