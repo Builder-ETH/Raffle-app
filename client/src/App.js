c8660b4797f22612d172cdc31e51f9c4a380c134
dsingh <dsingh@commvault.com>
2022-06-16 10:00:44 +0530

    remove gitignore files

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,16 @@ import Home from "./pages/home";
 import "./App.css";
 import Raffles from "./pages/Raffles";
 import modalForm from "./pages/eventAddModal";
+import { Routes, Route } from "react-router-dom"
+import Participate from "./pages/participate"; 
 
 const App = () => {
 
   return (
-    <>
-    <modalForm />
-    <Home>
-      <modalForm />
-    </Home>
-    <Raffles />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/participate" element={<Participate/>}/>
+    </Routes>
   )
 }
 export default App;
